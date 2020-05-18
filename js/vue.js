3c68e38f24aef9bb15d216bbb4439a9f9f39ec5e
bstojanovski <bojan.stojanovski@comeon.com>
2020-05-19 00:24:38 +0200

    Formatted vue.js file

--- js/vue.js
+++ js/vue.js
@@ -1,15 +1,29 @@
-﻿new Vue({
-    el: '#app',
-    data () {
-      return {
-        data: null
-      }
-    },
-    mounted () {
-      // Getting data from API
-      axios
-        .get('https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga')
-        .then(response => (this.data = response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']))
-        .catch(error => console.log(error));
-    }
-  })
+﻿new Vue({
+  el: "#app",
+
+  data() {
+    return {
+      data: null,
+    };
+  },
+
+  mounted() {
+    // Getting data from API
+
+    axios
+
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga"
+      )
+
+      .then(
+        response =>
+          (this.data =
+            response.data._embedded["viaplay:blocks"][0]._embedded[
+              "viaplay:products"
+            ])
+      )
+
+      .catch(error => console.log(error));
+  },
+});
